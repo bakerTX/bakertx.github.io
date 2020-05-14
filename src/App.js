8b6218cd1e3106465919ec3ad6a73db10583aceb
Aaron Baker <aaron.baker@ibm.com>
2020-05-13 20:11:52 -0500

    refactor and simplify content

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 import React from 'react';
 
 import './home.css';
-import photo from './Astro3952_rectangle.jpg';
-
-import Codepens from './Codepens';
+import './theme.css';
+import './util.css';
 
 function App() {
   return (
-    <div className="everything">
+    <div className="master-container">
       <header>
-        <h1 className="hero-text">Hey, I&#39;m Aaron. I&#39;m a front end web engineer working in Austin, TX.</h1>
-        <p>I specialize in translating visual designs into responsive and performant web applications, using React. I&#39;m also quite passionate about web accessibility.</p>
+        <h1 className="hero-text">
+          Hey, I&#39;m Aaron. I&#39;m a front end web engineer working in
+          Austin, TX.
+        </h1>
+        <p>
+          I specialize in translating visual designs into responsive and
+          performant web applications, using React and other tools. I&#39;m also
+          quite interested in web accessibility.
+        </p>
 
-        <p>In my spare time, I like to learn new technologies. Recently, I&#39;ve been hacking on a Raspberry Pi, and exploring a variety of Linux distributions. I also like to cook, and I&#39;m currently training for a half marathon in Alaska.</p>
+        <p>
+          In my spare time, I like to learn new technologies. Recently, I&#39;ve
+          been hacking on a Raspberry Pi, and exploring a variety of Linux
+          distributions. I also like to cook, and I&#39;m currently training for
+          a half marathon in Alaska.
+        </p>
 
-        <p>As you&#39;ll eventually come to know, I&#39;m a huge fan of everything space related.</p>
+        <p>
+          As you&#39;ll eventually come to know, I&#39;m a huge fan of
+          everything space related.
+        </p>
       </header>
-
-      <div className="util-column util-centered">
-        <img src={photo} alt="artistic depiction of an astronaut" className="hero-image" />
-        <span>
-          Photo credit
-          {'  '}
-          <a href="https://heryzhuhaer.wixsite.com/hezu">Hezu</a>
-        </span>
-      </div>
-
-      <h2>Codepen</h2>
-      <Codepens />
-
+      <h2>Projects</h2>
       <h2>Contact me</h2>
       <p>
-        Feel free to shoot me an email and I will get back to you as soon as I can.
+        Feel free to shoot me an email and I will get back to you as soon as I
+        can.
         {' '}
         <address>
           <a href="mailto:aaronbaker9210@gmail.com">aaronbaker9210@gmail.com</a>
