7fec51606d3bbebe5a6f7f345ee28283a6ed2af6
Aaron Baker <aaronbaker9210@gmail.com>
2020-05-26 18:36:57 -0500

    refactors content and adds rotating interest

--- src/App.js
+++ src/App.js
@@ -5,41 +5,56 @@ import './theme.css';
 import './util.css';
 
 function App() {
+  const rotatingInterests = [
+    'web accessibility',
+    'website discoverability with SEO',
+    'flexbox',
+    'server side rendering',
+  ];
+
+  const random = Math.floor(Math.random() * rotatingInterests.length);
   return (
     <div className="master-container">
-      <header>
-        <h1 className="hero-text">
-          Hey, I&#39;m Aaron. I&#39;m a front end web engineer working in
-          Austin, TX.
-        </h1>
-        <p>
-          I specialize in translating visual designs into responsive and
-          performant web applications, using React and other tools. I&#39;m also
-          quite interested in web accessibility.
-        </p>
+      <div className="flex-item">
+        <header>
+          <h1 className="hero-text">
+            Aaron Baker is a front end web engineer working in Austin, TX.
+          </h1>
+          <p>---</p>
+          <p>
+            I specialize in translating visual designs into responsive and
+            sensible user interfaces, using CSS and React.
+          </p>
 
-        <p>
-          In my spare time, I like to learn new technologies. Recently, I&#39;ve
-          been hacking on a Raspberry Pi, and exploring a variety of Linux
-          distributions. I also like to cook, and I&#39;m currently training for
-          a half marathon in Alaska.
-        </p>
+          <p>
+            I&#39;ve worked on the front end of enterprise-level software
+            applications for 4 years.
+          </p>
 
+          <p>
+            Right now, I&#39;m learning more about
+            {' '}
+            <span className="rotating-interest">
+              {rotatingInterests[random]}
+              .
+            </span>
+          </p>
+          <p>---</p>
+        </header>
+        <h2>Contact me</h2>
         <p>
-          As you&#39;ll eventually come to know, I&#39;m a huge fan of
-          everything space related.
+          If you&#39;re interested in collaborating on a project, or in
+          discussing my current work availability, please contact me with an
+          email at
+          {'  '}
+          <address className="inline-address">
+            <a href="mailto:aaronbaker9210@gmail.com">
+              aaronbaker9210@gmail.com
+            </a>
+          </address>
+          .
         </p>
-      </header>
-      <h2>Projects</h2>
-      <h2>Contact me</h2>
-      <p>
-        Feel free to shoot me an email and I will get back to you as soon as I
-        can.
-        {' '}
-        <address>
-          <a href="mailto:aaronbaker9210@gmail.com">aaronbaker9210@gmail.com</a>
-        </address>
-      </p>
+      </div>
     </div>
   );
 }
