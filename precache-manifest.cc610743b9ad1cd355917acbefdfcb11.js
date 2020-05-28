4decc0d32852a95612bcb55b2c455499f1f43719
Aaron Baker <aaronbaker9210@gmail.com>
2020-05-28 08:58:38 -0500

    Updates

---
+++ precache-manifest.cc610743b9ad1cd355917acbefdfcb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457380fd4b24cc178d6c8d2d66b41fb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa6d2425068a10dbf16",
+    "url": "/static/css/main.ffd9352c.chunk.css"
+  },
+  {
+    "revision": "e52363c6e4d9317645f1",
+    "url": "/static/js/2.78713701.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.78713701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa6d2425068a10dbf16",
+    "url": "/static/js/main.f450e638.chunk.js"
+  },
+  {
+    "revision": "9e33703e288be69e551c",
+    "url": "/static/js/runtime-main.8f9be27d.js"
+  }
+]);
