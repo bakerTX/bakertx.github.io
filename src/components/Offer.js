210b564812a59c6ad9ca8d7cc0d0bfef626a24e4
Aaron Baker <aaronbaker9210@gmail.com>
2020-06-22 22:07:28 -0500

    add source map explorer, fix typo

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -10,10 +10,10 @@ const Offer = () => (
     <div className="offer__container">
       <h3>Like this site?</h3>
       <p className="offer__description">
-        I decided to start offering my websites to the world. I&#39;m offering a fast, responsive, and quickly deployed website for individuals who are looking for a personal portfolio, or any sort of lightweight web presence.
+        I decided to start offering my websites to the world. I&#39;m offering a fast, responsive, and quickly deployed website for individuals in need of a personal portfolio or any sort of lightweight web presence.
       </p>
       <p className="offer__description">
-        My favorite websites are hand-made and made with care. While I can&#39;t offer you the next world class Amazon competitor, I can offer a solid and reponsive site that is customized to your needs.
+        My favorite websites are hand made and made with care. While I can&#39;t offer you the next world class Amazon competitor, I can offer a solid and responsive website that is customized to your needs.
       </p>
       <p className="offer__description">
         If you&#39;re interested, let&#39;s talk. You can find me at
