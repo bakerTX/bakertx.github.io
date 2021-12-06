291b687037d1bc534e30d574ea7a035b69418994
Aaron Baker <aaronbaker9210@gmail.com>
2021-12-06 00:23:54 -0800

    set up static site generator for simple dec-2021 portfolio

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+    // Return your Object options:
+    eleventyConfig.addPassthroughCopy("styles.css");
+    return {
+      dir: {
+        input: "pages",
+        output: "_site"
+      }
+    }
+  };
